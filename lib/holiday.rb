64a33d3cd284e3de96a6c425d49fe917e22612f0
Rocio Batres <jonasgirl21@gmail.com>
2020-09-17 02:22:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,10 +70,10 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |event, supplies|
       if supplies.include?("BBQ")
-        event.flatten
+        event
       end   
     end  
-  end 
+  end.flatten 
 end
 
 
